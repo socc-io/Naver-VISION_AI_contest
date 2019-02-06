47d141c37d966c915b16747e6d9bc3ac1b4e0bdc
Seohyun Back <becxer87@gmail.com>
2019-02-07 00:36:05 +0900

    fix inference count bug

--- inference.py
+++ inference.py
@@ -8,8 +8,6 @@ from delf import feature_io
 
 
 def get_feature(queries, references, sess, batch_size):
-    queries = np.asarray(queries)
-    references = np.asarray(references)
 
     query_dataset = tf.data.Dataset.from_generator(
         lambda:image_generator(queries),
@@ -34,27 +32,25 @@ def get_feature(queries, references, sess, batch_size):
     feature_vector = graph.get_tensor_by_name("feature_vector:0")
     input_x = graph.get_tensor_by_name("input_X1:0")
 
-    try:
-        while True:
+    while True:
+        try:
             query_imgs = sess.run(query_img)
             feed_dict = {input_x: query_imgs}
             query_vecs = sess.run(feature_vector, feed_dict)
             total_query_vecs.extend(query_vecs)
+        except tf.errors.OutOfRangeError:
+            print("query[%d/%d] inference complete" % (len(total_query_vecs), len(queries)))
+            break
 
-    except tf.errors.OutOfRangeError:
-        print("query[%d/%d] inference complete" % (len(query_vecs), len(queries)))
-        pass
-
-    try:
-        while True:
+    while True:
+        try:
             reference_imgs = sess.run(reference_img)
             feed_dict = {input_x: reference_imgs}
             reference_vecs = sess.run(feature_vector, feed_dict)
             total_reference_vecs.extend(reference_vecs)
-
-    except tf.errors.OutOfRangeError:
-        print("reference[%d/%d] inference complete" % (len(reference_vecs), len(references)))
-        pass
+        except tf.errors.OutOfRangeError:
+            print("reference[%d/%d] inference complete" % (len(total_reference_vecs), len(references)))
+            break
 
     total_query_vecs = np.asarray(total_query_vecs)
     total_reference_vecs = np.asarray(total_reference_vecs)
